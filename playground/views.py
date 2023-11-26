3557468aad763db4a38b37077f4f20391067bea5
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-11-26 22:03:43 +0530

    E4: Added a templates

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('Helo')
+    return render(request, 'hello.html', {'name': 'Rambo'})
