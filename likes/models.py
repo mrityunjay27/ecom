e6a17aa5e7ab00b55bde3ac855e730b4fd42cd92
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-11-28 23:56:45 +0530

    E10: Defined a Generic relationship and added an app 'likes'

--- likes/models.py
+++ likes/models.py
@@ -1,18 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-# Create your models here.
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
 
-class TaggedItem(models.Model):
-    # What tag applied to what object
-    # 3 things has to be defined for Generic Relationships
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # TYPE (product, video, article)
+
+# Create your models here.
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     # id
     object_id = models.PositiveIntegerField()
     # object
     content_object = GenericForeignKey()  # this will fetch the actual content
-
