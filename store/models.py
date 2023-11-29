afc8e58c6d22e92b24d7741f35cd671ad8632dbf
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-11-30 00:12:40 +0530

    E17: fixed model and pushed some data

--- store/models.py
+++ store/models.py
@@ -41,7 +41,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
-    birth_data = models.DateField(null=True)
+    birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
