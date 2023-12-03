b2612cd50a79744f0208e9e18c7adfb2412bb655
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-12-03 20:05:43 +0530

    E29: Querying generic relationships

--- playground/views.py
+++ playground/views.py
@@ -4,6 +4,8 @@ from store.models import Product, OrderItem, Order, Customer
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.aggregates import Count, Max, Sum, Avg
+from django.contrib.contenttypes.models import ContentType
+from tags.models import TaggedItem
 
 
 # Create your views here.
@@ -105,7 +107,8 @@ def say_hello(request):
     qs20 = Product.objects.prefetch_related('promotions').select_related('collection').all()
 
     # Ques: Get the last 5 orders with their customers and items (incl product)
-    qs21 = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    qs21 = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[
+           :5]
 
     # Aggregates-> all functions returns dict
     # Count return a dictionary {'id__count': 1000},
@@ -138,5 +141,17 @@ def say_hello(request):
     # Expression wrapper
     discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
     qs27 = Customer.objects.annotate(discounted_price=discounted_price)
+
+    # Querying Generic relationships ( Tags)
+    # We need to fetch id of Model from content_type table upon which we will query (say suppose Product)
+    # content type id for product model
+    content_type = ContentType.objects.get_for_model(Product)
+    # Now, query actual object, Give tags for given product
+    qs28 = TaggedItem.objects \
+        .select_related('tag') \
+        .filter(
+            content_type=content_type,
+            object_id=1  # 1 is product with id 1
+        )
 
     return render(request, 'hello.html', {'name': 'Rambo', 'data': qs22})
