ca763a20ee8699e0d919c4632bc8d739a0e6b582
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-12-10 00:56:04 +0530

    Admin: Select related and Order admin interface

--- store/admin.py
+++ store/admin.py
@@ -5,15 +5,21 @@ from . import models
 # Register your models here.
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    # Reduces number of query by preloading
+    # because we are doing product.collection.title
+    list_select_related = ['collection']
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
             return 'Low'
         return 'OK'
+
+    def collection_title(self, product):
+        return product.collection.title
 
 
 @admin.register(models.Customer)
@@ -22,6 +28,12 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    list_per_page = 10
 
 
 # By registering model to admin we can access it from admin interface
--- store/models.py
+++ store/models.py
@@ -57,10 +57,10 @@ class Customer(models.Model):
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     def __str__(self) -> str:
-        return self.first_name
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        ordering = ['first_name']
+        ordering = ['first_name', 'last_name']
 
 
 class Address(models.Model):
