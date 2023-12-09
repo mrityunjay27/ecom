8f8f9b3b8d3400f216eedb98a90e36883e1da6b2
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-12-10 00:37:33 +0530

    Admin: Other way of registering and customizing the columns in the admin interface

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+
 
 # By registering model to admin we can access it from admin interface
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
+#  admin.site.register(models.Product)
--- store/models.py
+++ store/models.py
@@ -56,6 +56,12 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+    def __str__(self) -> str:
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
