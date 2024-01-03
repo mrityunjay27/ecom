10021d8d1e6a90d1c90250be6df044c3318186fa
mrityunjay27 <mrityunjay27062000@gmail.com>
2024-01-04 00:37:53 +0530

    Rest 5: Adding relationships to serializer Method 3 HyperLink

--- store/views.py
+++ store/views.py
@@ -19,7 +19,7 @@ def product_list(request):
     :return:
     """
     queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True)
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 
@@ -45,3 +45,8 @@ def product_detail(request, id):
     # product = get_object_or_404(Product, pk=id)
     # serializer = ProductSerializer(product)
     # return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response("ok")
