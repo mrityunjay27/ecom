43d699b7638cc2ac5a018b314cb65289ed7bb2c0
mrityunjay27 <mrityunjay27062000@gmail.com>
2024-01-13 18:42:41 +0530

    Rest 18: Routers

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,23 @@
+from pprint import pprint
 from django.urls import path
 from . import views
+from rest_framework.routers import SimpleRouter, DefaultRouter
+
+router = DefaultRouter()  # with this we can hit http://127.0.0.1:8000/store
+# router = SimpleRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+# pprint(router.urls)  # This contains url patterns, will generate 4 endpoint as defined below.
+
 
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
-]
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>/', views.ProductDetail.as_view()),
+#     path('collections/', views.CollectionList.as_view()),
+#     path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
+# ]
